81fc52e7887c1772f51aeda4ec623fb0b3a3bea3
shawnwildermuth <shawn@wildermuth.com>
2024-07-31 14:26:56 -0400

    That Conference

---
+++ ThatConference/2024/SourceGenerators/ClassListConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using static System.Console;
+
+//var classTypes = Assembly.GetEntryAssembly()?
+//  .GetTypes()
+//  .Where(t => t.IsClass && t.IsPublic)
+//  .ToList();
+
+//if (classTypes is not null)
+//{
+  foreach (var c in ClassListGenerator.ClassList.Names)
+  {
+    WriteLine(c);
+  }
+//}
+
+
+public class AnotherSillyClass { }
---
+++ ThatConference/2024/SourceGenerators/ClassListGenerator/TheGenerator.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace ClassListGenerator;
+
+[Generator]
+public class TheGenerator : IIncrementalGenerator
+{
+  public void Initialize(
+    IncrementalGeneratorInitializationContext context)
+  {
+    var provider = context.SyntaxProvider.CreateSyntaxProvider(
+      predicate: static (node, _) => node is ClassDeclarationSyntax,
+      transform: static (ctx, _) => (ClassDeclarationSyntax)ctx.Node
+      )
+      .Where(static n => n is not null);
+
+    var compilation = context.CompilationProvider.Combine(provider.Collect());
+
+    context.RegisterSourceOutput(compilation, Execute);
+  }
+
+  static void Execute(SourceProductionContext context, 
+    (Compilation Left, ImmutableArray<ClassDeclarationSyntax> Right) tuple)
+  {
+    var (compilation, list) = tuple;
+
+    var items = new List<string>();
+
+    foreach (var syntax in list)
+    {
+      var symbol = compilation
+        .GetSemanticModel(syntax.SyntaxTree)
+        .GetDeclaredSymbol(syntax) as INamedTypeSymbol;
+
+      items.Add(@$"""{symbol.ToDisplayString()}""");
+    }
+
+    var joined = string.Join(", ", items);
+
+    var code = $$"""
+      namespace ClassListGenerator;
+
+      public class ClassList
+      {
+        public static List<string> Names = new()
+        {
+           {{ joined }}
+        };
+      }
+      """;
+
+    context.AddSource("YourClassList.g.cs", code);
+  }
+}
